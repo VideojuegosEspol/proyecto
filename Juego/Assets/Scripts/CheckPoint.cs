3b8fe40f120c29617303efc05b01061fd5496691
Roger Avilés G <rogiavil@espol.edu.ec>
2019-12-24 21:43:17 -0500

    Agregado posible CheckPoint

---
+++ Juego/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+
+    private GameMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>(); 
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) //Se activa el CheckPoint si colisiona con el jugador
+        {
+            gm.lastCheckPoint = transform.position;
+        }
+    }
+
+    //https://www.youtube.com/watch?v=ofCLJsSUom0&list=WL&index=2&t=0s
+}
---
+++ Juego/Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Aqui debe entrar un if cuando el player muera para cargar CheckPoint
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
