9923022b1146cc670cc79f58f9093cdc3cbf4432
juanj97 <juanj121297e@gmail.com>
2019-12-24 23:05:14 -0500

    no me deja subir ayudaaa

---
+++ Juego/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 15f;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name );
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+        
+    }
+
+}
